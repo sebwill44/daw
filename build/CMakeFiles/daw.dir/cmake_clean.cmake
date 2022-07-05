6d226f53676949ecea61ef825f0efc2975c9970b
sebwill44 <svw9@duke.edu>
2022-07-04 23:09:07 -0400

    Initialized repo

---
+++ build/CMakeFiles/daw.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/daw.dir/main.cpp.o"
+  "CMakeFiles/daw.dir/main.cpp.o.d"
+  "daw"
+  "daw.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/daw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
